6c162a5be227112bc0bcb9c3c48e40d16c1380d7
松田省吾 <muvs1018@gmail.com>
2020-10-05 17:03:41 +0900

    #3 管理者画面機能追加

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -12,6 +12,33 @@ class Admin extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('admin');
+		$topics = $this->admin_model->get_topics();
+		foreach ($topics as $topic){
+			$data['clean_topics'][] = $this->security->xss_clean($topic);
+		}
+		$this->load->view('admin', $data);
+	}
+
+	public function get_post_data()
+	{
+		if($_SERVER['REQUEST_METHOD'] === 'POST'){
+			$id = $this->input->post('topic_id', true)? : null;
+			$post_data = $this->admin_model->get_post_data($id);
+			foreach ($post_data as $key_num => $data){
+				foreach ($data as $key_name => $value){
+					$clean_post_data[$key_num][$key_name] = $this->security->xss_clean($value);
+				}
+			}
+			echo json_encode($clean_post_data, JSON_UNESCAPED_UNICODE);
+		}else{
+			header('location: ../');
+			exit;
+		}
+	}
+
+	public function post_delete()
+	{
+		$id = $this->input->post('post_id', true)? : null;
+		$this->admin_model->post_delete($id);
 	}
 }
--- application/views/admin.php
+++ application/views/admin.php
@@ -1,23 +1,14 @@
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
-<html lang="en">
-
+<html lang="ja">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-
-  <title>AdminLTE 3 | Starter</title>
+  <title>K-G-Ban ADMIN</title>
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="./AdminLTE/plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="./AdminLTE/dist/css/adminlte.min.css">
-  <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 
@@ -29,120 +20,57 @@ scratch. This page gets rid of all links and provides the needed markup only.
     text-overflow: ellipsis;
     white-space: nowrap;
   }
+
+  .delete_btn {
+    width: 100%;
+  }
 </style>
 
-<body class="hold-transition sidebar-mini">
-  <div class="wrapper">
-
-    <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Brand Logo -->
-      <a href="index3.html" class="brand-link">
-        <span class="brand-text font-weight-light">ADMIN</span>
-      </a>
-
-      <!-- Sidebar -->
-      <div class="sidebar">
-        <!-- Sidebar Menu -->
-        <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-              with font-awesome or any other icon font library -->
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon"></i>
-                <p>
-                  リスト
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon"></i>
-                <p>
-                  編集
-                </p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon"></i>
-                <p>
-                  削除
-                </p>
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <!-- /.sidebar-menu -->
+<body>
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-12">
+          <h1 class="m-0 text-dark">管理画面</h1>
+          <div style="display:flex;">
+            <div class="form-group" style="margin: 30px;">
+              <select name="bbsName" id="topic" class="form-control" style="width: 400px;">
+                <option value="#">トピックを選択してください。</option>
+                <?php foreach ($clean_topics as $clean_topic): ?>
+                  <option value="<?= $clean_topic['id']; ?>"><?= $clean_topic['name']; ?></option>
+                <?php endforeach; ?>
+              </select>
+            </div>
+            <button type="button" id="topic_btn" class="btn btn-primary" style="width:100px; height: 40px; margin-top: 28px;">表示</button>
+          </div>
+          <table class="table table-striped table-bordered" id="lingTable">
+            <thead style="text-align: center">
+              <tr>
+                <th>番号</th>
+                <th>名前</th>
+                <th>本文</th>
+                <th>画像</th>
+                <th>削除パス</th>
+                <th>削除</th>
+              </tr>
+            </thead>
+            <tbody id="post"></tbody>
+          </table>
+        </div>
       </div>
-      <!-- /.sidebar -->
-    </aside>
-
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <div class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
-            <div class="col-sm-12">
-              <h1 class="m-0 text-dark">管理者画面</h1>
-              <div style="display:flex;">
-                <div class="form-group" style="margin: 30px;">
-                  <select name="bbsName" id="" class="form-control" style="width: 400px;">
-                    <option value="">掲示板1</option>
-                    <option value="">掲示板2</option>
-                    <option value="">掲示板3</option>
-                    <option value="">掲示板4</option>
-                    <option value="">掲示板5</option>
-                  </select>
-                </div>
-                <button type="button" class="btn btn-primary" style="width:100px; height: 40px; margin-top: 28px;">表示</button>
-              </div>
-              <table class="table table-striped table-bordered" id="lingTable">
-                <thead style="text-align: center">
-                  <tr>
-                    <th>番号</th>
-                    <th>名前</th>
-                    <th>本文</th>
-                    <th>削除パス</th>
-                    <th>削除</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td>松田省吾</td>
-                    <td class="box-read">あああああああああああああああああああああああああああああああああああ</td>
-                    <td>aaaaaa</td>
-                    <td style="text-align: center; width: 100px;"><button id="delete_btn" type="button" class="btn btn-danger" style="width: 100px;">削除</button></td>
-                  </tr>
-                </tbody>
-              </table>
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content-header -->
     </div>
   </div>
-  <!-- ./wrapper -->
 
-  <!-- REQUIRED SCRIPTS -->
-
-  <!-- jQuery -->
   <script src="./AdminLTE/plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
   <script src="./AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
   <script src="./AdminLTE/dist/js/adminlte.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
   <script>
+    //データテーブル生成
     $(document).ready(function() {
       $('#lingTable').DataTable({
         "language": {
@@ -150,21 +78,57 @@ scratch. This page gets rid of all links and provides the needed markup only.
         },
       });
     });
+
+    //投稿削除
+    function post_delete(id) {
+      swal({
+          title: "削除してもよろしいですか？",
+          icon: "warning",
+          buttons: true,
+          dangerMode: true,
+        })
+        .then((willDelete) => {
+          if (willDelete) {
+            $.ajax({
+                url: './Admin/post_delete',
+                type: 'POST',
+                data: {
+                  'post_id': id
+                }
+              })
+              .done((data) => {
+                console.log(data);
+              })
+            $('#' + id).hide();
+            swal("投稿を削除しました。", "", "success");
+          } else {
+            swal("キャンセルしました。", "", "success");
+          }
+        });
+    }
+
+    //投稿データ切り替え
     $(function() {
-      $('#delete_btn').on('click', function() {
-        swal({
-            title: "削除してもよろしいですか？",
-            icon: "warning",
-            buttons: true,
-            dangerMode: true,
-          })
-          .then((willDelete) => {
-            if (willDelete) {
-              swal("投稿を削除しました。", "", "success");
-            } else {
-              swal("キャンセルしました。", "", "success");
+      $('#topic_btn').on('click', function() {
+        $.ajax({
+            url: './Admin/get_post_data',
+            type: 'POST',
+            data: {
+              'topic_id': $('#topic').val()
             }
-          });
+          })
+          .done((data) => {
+            console.log(data);
+            const tr = document.getElementById('post')
+            const obj = JSON.parse(data);
+            while (tr.firstChild) {
+              tr.removeChild(tr.firstChild);
+            }
+            obj.forEach(function(value) {
+              $('#post').append("<tr id=\"" + value['id'] + "\"><td>" + value['id'] + "</td><td>" + value['name'] + "</td><td class=\"box-read\">" + value['text'] +
+                "</td><td>" + value['img'] + "</td><td>" + value['delete_pass'] + "</td><td><button class=\"delete_btn btn btn-danger\" onclick=\"post_delete(" + value['id'] + ")\">削除</button></td>");
+            })
+          })
       })
     })
   </script>
